6539139e69467293830ac915cdea4498d10598b8
2022bcs0138-nissi <nissi22bcs138@iiitkottayam.ac.in>
2026-01-09 15:07:39 +0530

    EXP-04: Random Forest Regressor (100 trees)

--- src/train.py
+++ src/train.py
@@ -6,6 +6,8 @@ import os
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Ridge
+from sklearn.ensemble import RandomForestRegressor
+
 
 
 
@@ -23,10 +25,10 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = Pipeline([
-    ("scaler", StandardScaler()),
-    ("ridge", Ridge(alpha=1.0))
-])
+model = RandomForestRegressor(
+    n_estimators=100,
+    random_state=42
+)
 
 model.fit(X_train, y_train)
 
