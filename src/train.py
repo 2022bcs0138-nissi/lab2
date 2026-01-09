a9396da163059dd748c4bd5eeb9b80ed372cf65b
2022bcs0138-nissi <nissi22bcs138@iiitkottayam.ac.in>
2026-01-09 15:00:58 +0530

    EXP-02: Ridge Regression (alpha=1.0), test split 0.2

--- src/train.py
+++ src/train.py
@@ -5,7 +5,7 @@ import os
 
 
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error, r2_score
 
 df = pd.read_csv("data/winequality-red.csv", sep=",")
@@ -18,7 +18,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = LinearRegression()
+model = Ridge(alpha=1.0)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
