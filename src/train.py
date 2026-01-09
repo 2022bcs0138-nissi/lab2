3f3b667321adc0df714fafc2b65d0eea172ea8d9
2022bcs0138-nissi <nissi22bcs138@iiitkottayam.ac.in>
2026-01-09 14:39:32 +0530

    Fix CSV delimiter issue in training script

--- src/train.py
+++ src/train.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-df = pd.read_csv("data/winequality-red.csv", sep=";")
+df = pd.read_csv("data/winequality-red.csv", sep=",")
 df.columns = df.columns.str.strip().str.lower()
 
 X = df.drop("quality", axis=1)
